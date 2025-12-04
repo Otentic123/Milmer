f595ea527c49f563867d1a9977faf1540aeed96f
Otentic123 <ignaciomilmer@gmail.com>
2025-12-04 08:27:55 +0800

    Update Milmer4.java

--- Ignacio/Milmer4.java
+++ Ignacio/Milmer4.java
@@ -7,8 +7,18 @@
  */
 public class Milmer4
 {
-    public static void main (String [] args){
+       public static void main (String [] args){
         int[] arr = {45, 12, 88, 5, 93, 22};
+        int smallest = arr[0];
+        int largest = arr[0];
+        
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < smallest) smallest = arr[i];
+            if (arr[i] > largest) largest = arr[i];
+        }
+        
+        System.out.println("Smallest: " + smallest);
+        System.out.println("Largest: "+ largest);
         
         
     }
